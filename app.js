0152af38f0e42d182c1f466064beea29df668da6
Tony <38567749+Fewsty@users.noreply.github.com>
2024-06-04 14:17:24 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -5,111 +5,78 @@ tg.expand();
 tg.MainButton.textColor = "#FFFFFF";
 tg.MainButton.color = "#2cab37";
 
-(function () {
-  // The width and height of the captured photo. We will set the
-  // width to the value defined here, but the height will be
-  // calculated based on the aspect ratio of the input stream.
+Telegram.WebApp.onEvent("mainButtonClicked", function () {
+  tg.sendData(item);
+});
 
-  var width = 320; // We will scale the photo width to this
-  var height = 0; // This will be computed based on the input stream
+let usercard = document.getElementById("usercard");
 
-  // |streaming| indicates whether or not we're currently streaming
-  // video from the camera. Obviously, we start at false.
+let p = document.createElement("p");
 
-  var streaming = false;
+p.innerText = `${tg.initDataUnsafe.user.first_name}
+${tg.initDataUnsafe.user.last_name}`;
 
-  // The various HTML elements we need to configure or control. These
-  // will be set by the startup() function.
+usercard.appendChild(p);
 
-  var video = null;
-  var canvas = null;
-  var photo = null;
-  var startbutton = null;
+var videoElement = document.querySelector("video");
+getStream().then(getDevices).then(gotDevices);
 
-  function startup() {
-    video = document.getElementById("video");
-    canvas = document.getElementById("canvas");
-    photo = document.getElementById("photo");
-    startbutton = document.getElementById("startbutton");
+function getDevices() {
+  // AFAICT in Safari this only gets default devices until gUM is called :/
+  return navigator.mediaDevices.enumerateDevices();
+}
 
-    navigator.mediaDevices
-      .getUserMedia({ video: true, audio: false })
-      .then(function (stream) {
-        video.srcObject = stream;
-        video.play();
-      })
-      .catch(function (err) {
-        console.log("An error occurred: " + err);
-      });
+function gotDevices(deviceInfos) {
+  window.deviceInfos = deviceInfos; // make available to console
+  console.log("Available input and output devices:", deviceInfos);
+}
+window.setTimeout(getStream, 10);
 
-    video.addEventListener(
-      "canplay",
-      function (ev) {
-        if (!streaming) {
-          height = video.videoHeight / (video.videoWidth / width);
-
-          // Firefox currently has a bug where the height can't be read from
-          // the video, so we will make assumptions if this happens.
-
-          if (isNaN(height)) {
-            height = width / (4 / 3);
+function getStream() {
+  if (window.stream) {
+    window.stream.getTracks().forEach((track) => {
+      track.stop();
+    });
+  }
+  const videoSource = videoSelect.value;
+  const constraints = {
+    video: {
+      deviceId: videoSource
+        ? {
+            exact: videoSource,
           }
+        : undefined,
+    },
+  };
+  return navigator.mediaDevices
+    .getUserMedia(constraints)
+    .then(gotStream)
+    .catch(handleError);
+}
 
-          video.setAttribute("width", width);
-          video.setAttribute("height", height);
-          canvas.setAttribute("width", width);
-          canvas.setAttribute("height", height);
-          streaming = true;
-        }
-      },
-      false
-    );
-
-    startbutton.addEventListener(
-      "click",
-      function (ev) {
-        takepicture();
-        ev.preventDefault();
-      },
-      false
-    );
-
-    clearphoto();
+function gotStream(stream) {
+  window.stream = stream; // make stream available to console
+  if ("srcObject" in videoElement) {
+    videoElement.srcObject = stream;
+  } else {
+    videoElement.src = URL.createObjectURL(stream);
   }
+}
 
-  // Fill the photo with an indication that none has been
-  // captured.
+function handleError(error) {
+  console.error("Error: ", error);
+}
 
-  function clearphoto() {
-    var context = canvas.getContext("2d");
-    context.fillStyle = "#AAA";
-    context.fillRect(0, 0, canvas.width, canvas.height);
-
-    var data = canvas.toDataURL("image/png");
-    photo.setAttribute("src", data);
-  }
-
-  // Capture a photo by fetching the current contents of the video
-  // and drawing it into a canvas, then converting that to a PNG
-  // format data URL. By drawing it on an offscreen canvas and then
-  // drawing that to the screen, we can change its size and/or apply
-  // other changes before drawing it.
-
-  function takepicture() {
-    var context = canvas.getContext("2d");
-    if (width && height) {
-      canvas.width = width;
-      canvas.height = height;
-      context.drawImage(video, 0, 0, width, height);
-
-      var data = canvas.toDataURL("image/png");
-      photo.setAttribute("src", data);
-    } else {
-      clearphoto();
-    }
-  }
-
-  // Set up our event listener to run the startup process
-  // once loading is complete.
-  window.addEventListener("load", startup, false);
-})();
+function getImgUrl() {
+  let el = videoElement;
+  let canvas = document.createElement("canvas");
+  canvas.width = el.videoWidth;
+  canvas.height = el.videoHeight;
+  canvas.style.display = "none";
+  document.body.appendChild(canvas);
+  canvas.getContext("2d").drawImage(el, 0, 0, canvas.width, canvas.height);
+  let cu = canvas.toDataURL();
+  document.body.removeChild(canvas);
+  return cu;
+}
+getImgUrl();
