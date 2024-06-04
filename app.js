b0ed643d66f7ffae829ce7fbf53dd86bf745775a
Tony <38567749+Fewsty@users.noreply.github.com>
2024-06-04 13:24:36 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,24 @@
-window.onload = (function () {
+let tg = window.Telegram.WebApp;
+
+tg.expand();
+
+tg.MainButton.textColor = "#FFFFFF";
+tg.MainButton.color = "#2cab37";
+
+Telegram.WebApp.onEvent("mainButtonClicked", function () {
+  tg.sendData(item);
+});
+
+let usercard = document.getElementById("usercard");
+
+let p = document.createElement("p");
+
+p.innerText = `${tg.initDataUnsafe.user.first_name}
+${tg.initDataUnsafe.user.last_name}`;
+
+usercard.appendChild(p);
+
+(function () {
   // The width and height of the captured photo. We will set the
   // width to the value defined here, but the height will be
   // calculated based on the aspect ratio of the input stream.
